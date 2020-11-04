3b45d444fdb5766a94a80a96fbbd9ff5d8dfe1fd
Marcin Kisielewski <mkisielewski25@gmail.com>
2020-11-04 21:24:40 +0100

    adding new tasks and showing if its done

--- js/script.js
+++ js/script.js
@@ -1,32 +1,57 @@
 {
-  const tasks = [
-    {
-      content: "wstać rano",
-      done: false,
+    const tasks = [{
+        content: "wstać rano",
+        done: false,
     },
     {
-      content: "wrócić z pracy",
-      done: true,
+        content: "wrócić z pracy",
+        done: true,
     },
-  ];
+    ];
 
-  const render = () => {
-    let htmlString = "";
+    const render = () => {
+        let htmlString = "";
 
-    for (const task of tasks) {
-      htmlString += `
-            <li>
+        for (const task of tasks) {
+            htmlString += `
+            <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
               ${task.content}
             </li>
             `
-    }
+        }
 
-    document.querySelector(".js-tasks").innerHTML = htmlString;
-  };
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
 
-  const init = () => {
-    render();
-  };
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
 
-  init();
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        };
+
+        addNewTask(newTaskContent);
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+
+
+    };
+
+    init();
 }
